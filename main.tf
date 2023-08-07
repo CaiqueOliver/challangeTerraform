dc2710d9b95b08cdd459821ef0e01714d6cbcb43
Caique Oliveira <tayko@live.com>
2023-08-06 21:06:07 -0300

    added variables to resources

--- main.tf
+++ main.tf
@@ -4,34 +4,38 @@ resource "azurerm_resource_group" "CAIQUE_OLIVEIRA_DA_SILVA" {
 }
 
 resource "azurerm_service_plan" "CAIQUE_OLIVEIRA_DA_SILVA" {
+  for_each            = var.app_service_config
   name                = var.app_name
   location            = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.location
   resource_group_name = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
-  os_type             = "Linux"
-  sku_name            = "F1"
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
 }
 
 resource "azurerm_linux_web_app" "CAIQUE_OLIVEIRA_DA_SILVA" {
+  for_each            = var.app_service_config
   name                = var.app_name
-  service_plan_id     = azurerm_service_plan.CAIQUE_OLIVEIRA_DA_SILVA.id
+  service_plan_id     = azurerm_service_plan.CAIQUE_OLIVEIRA_DA_SILVA[each.key]
   location            = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.location
   resource_group_name = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
   site_config {}
 }
 
 resource "azurerm_storage_account" "CAIQUE_OLIVEIRA_DA_SILVA" {
+  for_each                 = var.storage_account
   name                     = var.app_name
-  resource_group_name      = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
   location                 = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "FileStorage"
+  resource_group_name      = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication
+  account_kind             = each.value.account_kind
 }
 
 resource "azurerm_container_registry" "CAIQUE_OLIVEIRA_DA_SILVA" {
+  for_each            = var.container_registry
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
   location            = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.location
-  sku                 = "Standard"
-  admin_enabled       = false
+  resource_group_name = azurerm_resource_group.CAIQUE_OLIVEIRA_DA_SILVA.name
+  sku                 = each.value.sku
+  admin_enabled       = each.value.admin_enabled
 }
--- variables.tf
+++ variables.tf
@@ -6,12 +6,40 @@ variable "provider_config" {
   }
 }
 
+variable "app_name" {
+  type = string
+  default = "caique-oliveira-da-silva"
+}
+
 variable "location" {
   type    = string
   default = "East US 2"
 }
 
-variable "app_name" {
-  type    = string
-  default = "caique-oliveira-da-silva"
+variable "storage_account" {
+  default = {
+    config = {
+      account_tier        = "Standard"
+      account_replication = "LRS"
+      account_kind        = "FileStorage"
+    }
+  }
+}
+
+variable "container_registry" {
+  default = {
+    config = {
+      sku           = "Standard"
+      admin_enabled = false
+    }
+  }
+}
+
+variable "app_service_config" {
+  default = {
+    config = {
+      os_type  = "Linux"
+      sku_name = "F1"
+    }
+  }
 }
